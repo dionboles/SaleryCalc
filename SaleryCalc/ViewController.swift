7af96971c1f3cb36c82aa0c1c87e31f295022689
Dion Boles <dion.boles@gmail.com>
2018-12-10 14:21:42 -0500

    Initial Commit

---
+++ SaleryCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaleryCalc
+//
+//  Created by Dion Boles on 12/10/18.
+//  Copyright © 2018 Dion Boles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
